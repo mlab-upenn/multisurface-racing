4fa03adaf3c686b66325d3a9660f9578af4440a7
Tomas Nagy <nagytom4@fel.cvut.cz>
2022-11-21 23:01:51 -0500

    Add dynamic model for MPC

--- Kinematic_MPC.py
+++ Kinematic_MPC.py
@@ -7,7 +7,7 @@ from regulators.pure_pursuit import *
 from regulators.path_follow_mpc import *
 from models.kinematic import KinematicModel
 from models.extended_kinematic import ExtendedKinematicModel
-from models.GP_model_ensembleing import GPEnsembleModel
+from models.dynamic import DynamicBicycleModel
 from helpers.closest_point import *
 import torch
 import gpytorch
@@ -26,17 +26,95 @@ class MPCConfigEXT:
     TK: int = 10  # finite time horizon length kinematic
 
     Rk: list = field(
-        default_factory=lambda: np.diag([0.000001, 100.0])
+        default_factory=lambda: np.diag([0.000000001, 10.0])
     )  # input cost matrix, penalty for inputs - [accel, steering_speed]
     Rdk: list = field(
-        default_factory=lambda: np.diag([0.0000001, 100.0])
+        default_factory=lambda: np.diag([0.000000001, 10.0])
     )  # input difference cost matrix, penalty for change of inputs - [accel, steering_speed]
     Qk: list = field(
-        default_factory=lambda: np.diag([13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0])
+        default_factory=lambda: np.diag([13.5, 13.5, 10.5, 0.0, 0.0, 0.0, 0.0])
         # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
     )  # state error cost matrix, for the next (T) prediction time steps
     Qfk: list = field(
-        default_factory=lambda: np.diag([13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0])
+        default_factory=lambda: np.diag([13.5, 13.5, 10.5, 0.0, 0.0, 0.0, 0.0])
+        # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
+    )  # final state error matrix, penalty  for the final state constraints
+    N_IND_SEARCH: int = 20  # Search index number
+    DTK: float = 0.01  # time step [s] kinematic
+    dlk: float = 3.0  # dist step [m] kinematic
+    LENGTH: float = 4.298  # Length of the vehicle [m]
+    WIDTH: float = 1.674  # Width of the vehicle [m]
+    LR: float = 1.50876
+    LF: float = 0.88392
+    WB: float = 0.88392 + 1.50876  # Wheelbase [m]
+    MIN_STEER: float = -0.4189  # maximum steering angle [rad]
+    MAX_STEER: float = 0.4189  # maximum steering angle [rad]
+    MAX_STEER_V: float = 3.2  # maximum steering speed [rad/s]
+    MAX_SPEED: float = 45.0  # maximum speed [m/s]
+    MIN_SPEED: float = 0.0  # minimum backward speed [m/s]
+    MAX_ACCEL: float = 11.5  # maximum acceleration [m/ss]
+    MAX_DECEL: float = -45.0  # maximum acceleration [m/ss]
+
+    MASS: float = 1225.887  # Vehicle mass
+
+
+@dataclass
+class MPCConfigKIN:
+    NXK: int = 4  # length of kinematic state vector: z = [x, y, vx, yaw angle]
+    NU: int = 2  # length of input vector: u = = [acceleration, steering speed]
+    TK: int = 20  # finite time horizon length kinematic
+
+    Rk: list = field(
+        default_factory=lambda: np.diag([0.01, 10])
+    )  # input cost matrix, penalty for inputs - [accel, steering_angle]
+    Rdk: list = field(
+        default_factory=lambda: np.diag([0.01, 10])
+    )  # input difference cost matrix, penalty for change of inputs - [accel, steering_angle]
+    Qk: list = field(
+        default_factory=lambda: 0.01 * np.diag([13.5, 13.5, 8.5, 0.0])
+        # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
+    )  # state error cost matrix, for the next (T) prediction time steps
+    Qfk: list = field(
+        default_factory=lambda: 0.01 * np.diag([13.5, 13.5, 8.5, 0.0])
+        # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
+    )  # final state error matrix, penalty  for the final state constraints
+    N_IND_SEARCH: int = 20  # Search index number
+    DTK: float = 0.01  # time step [s] kinematic
+    dlk: float = 3.0  # dist step [m] kinematic
+    LENGTH: float = 4.298  # Length of the vehicle [m]
+    WIDTH: float = 1.674  # Width of the vehicle [m]
+    LR: float = 1.50876
+    LF: float = 0.88392
+    WB: float = 0.88392 + 1.50876  # Wheelbase [m]
+    MIN_STEER: float = -0.4189  # maximum steering angle [rad]
+    MAX_STEER: float = 0.4189  # maximum steering angle [rad]
+    MAX_STEER_V: float = 3.2  # maximum steering speed [rad/s]
+    MAX_SPEED: float = 45.0  # maximum speed [m/s]
+    MIN_SPEED: float = 0.0  # minimum backward speed [m/s]
+    MAX_ACCEL: float = 11.5  # maximum acceleration [m/ss]
+    MAX_DECEL: float = -45.0  # maximum acceleration [m/ss]
+
+    MASS: float = 1225.887  # Vehicle mass
+
+
+@dataclass
+class MPCConfigDYN:
+    NXK: int = 7  # length of kinematic state vector: z = [x, y, vx, yaw angle, vy, yaw rate, steering angle]
+    NU: int = 2  # length of input vector: u = = [acceleration, steering speed]
+    TK: int = 40  # finite time horizon length kinematic
+
+    Rk: list = field(
+        default_factory=lambda: np.diag([0.000000001, 10.0])
+    )  # input cost matrix, penalty for inputs - [accel, steering_speed]
+    Rdk: list = field(
+        default_factory=lambda: np.diag([0.000000001, 10.0])
+    )  # input difference cost matrix, penalty for change of inputs - [accel, steering_speed]
+    Qk: list = field(
+        default_factory=lambda: np.diag([13.5, 13.5, 5.5, 0.0, 0.0, 0.0, 0.0])
+        # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
+    )  # state error cost matrix, for the next (T) prediction time steps
+    Qfk: list = field(
+        default_factory=lambda: np.diag([13.5, 13.5, 5.5, 0.0, 0.0, 0.0, 0.0])
         # [13.5, 13.5, 5.5, 13.0, 0.0, 0.0, 0.0]
     )  # final state error matrix, penalty  for the final state constraints
     N_IND_SEARCH: int = 20  # Search index number
@@ -55,7 +133,23 @@ class MPCConfigEXT:
     MAX_ACCEL: float = 11.5  # maximum acceleration [m/ss]
     MAX_DECEL: float = -45.0  # maximum acceleration [m/ss]
 
+    # model parameters
     MASS: float = 1225.887  # Vehicle mass
+    I_Z: float = 1560.3729  # Vehicle inertia
+    TORQUE_SPLIT: float = 0.0  # Torque distribution
+
+    BR: float = 24.9504  # Pacejka tire model parameter B - rear tire
+    CR: float = 1.3754  # Pacejka tire model parameter C - rear tire
+    DR: float = 3702.9280  # Pacejka tire model parameter D - rear tire
+
+    BF: float = 9.4246  # Pacejka tire model parameter B - front tire
+    CF: float = 5.9139  # Pacejka tire model parameter C - front tire
+    DF: float = 3714.8218  # Pacejka tire model parameter D - front tire
+
+    # https://arxiv.org/pdf/1905.05150.pdf - equation (7)
+    CM: float = 0.9459
+    CR0: float = 2.3451
+    CR2: float = -0.0095
 
 
 def draw_point(e, point, colour):
@@ -92,12 +186,22 @@ def main():  # after launching this you can run visualization.py to see the resu
     """
 
     # Choose program parameters
-    model_to_use = 'pure_pursuit'  # options: ext_kinematic, pure_pursuit
-    map_name = 'rounded_rectangle'  # Nuerburgring, SaoPaulo, rounded_rectangle, l_shape
+    model_to_use = 'dynamic'  # options: ext_kinematic, pure_pursuit, kinematic, dynamic
+    map_name = 'Nuerburgring'  # Nuerburgring, SaoPaulo, rounded_rectangle, l_shape, BrandsHatch, DualLaneChange
+    rotate_map = False  # !!!! If the car is spawning with bad orientation change value here !!!! TODO Fix here so this is not needed anymore
     use_dyn_friction = False
-    control_step = 100.0  # ms
-    render_every = 1  # render graphics every n control steps
+    constant_friction = 0.5
+    control_step = 30.0  # ms
+    render_every = 40  # render graphics every n simulation steps
     constant_speed = True
+    constant_speed_value = 8.0
+    velocity_profile_multiplier = 0.9
+    number_of_laps = 5
+
+    ekin_config = MPCConfigEXT()
+    kin_config = MPCConfigKIN()
+    dyn_config = MPCConfigDYN()
+    ekin_config.DTK = kin_config.DTK = dyn_config.DTK = control_step / 1000.0
 
     # Creating the single-track Motion planner and Controller
 
@@ -105,16 +209,26 @@ def main():  # after launching this you can run visualization.py to see the resu
     work = {'mass': 1225.88, 'lf': 0.80597534362552312, 'tlad': 10.6461887897713965, 'vgain': 1.0}
 
     # Load map config file
-    with open('config_%s.yaml' % map_name) as file:
+    with open('configs/config_%s.yaml' % map_name) as file:
         conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
 
     if use_dyn_friction:
-        tpamap_name = './maps/rounded_rectangle/rounded_rectangle_tpamap.csv'
-        tpadata_name = './maps/rounded_rectangle/rounded_rectangle_tpadata.json'
+        if map_name == 'l_shape':
+            tpamap_name = './maps/l_shape/friction_data/l_shape_l720_track_tpamap.csv'
+            tpadata_name = './maps/l_shape/friction_data/l_shape_l720_track_tpadata.json'
+        if map_name == 'DualLaneChange':
+            # tpamap_name = './maps/DualLaneChange/friction_data/DualLaneChange_track_tpamap.csv'
+            # tpamap_name = './maps/DualLaneChange/friction_data/DualLaneChange5z_track_tpamap.csv'
+            tpamap_name = './maps/DualLaneChange/friction_data/DualLaneChange3zv2_track_tpamap.csv'
+            # tpadata_name = './maps/DualLaneChange/friction_data/DualLaneChange_track_tpadata.json'
+            # tpadata_name = './maps/DualLaneChange/friction_data/DualLaneChange5z_track_tpadata.json'
+            tpadata_name = './maps/DualLaneChange/friction_data/DualLaneChange3zv2_track_tpadata.json'
+        if map_name == 'SaoPaulo':
+            tpamap_name = './maps/SaoPaulo/friction_data/SaoPaulo_track_tpamap.csv'
+            tpadata_name = './maps/SaoPaulo/friction_data/SaoPaulo_track_tpadata.json'
 
         tpamap = np.loadtxt(tpamap_name, delimiter=';', skiprows=1)
-        tpamap *= 1.5  # map is 1.5 times larger than normal
 
         tpadata = {}
         with open(tpadata_name) as f:
@@ -123,9 +237,14 @@ def main():  # after launching this you can run visualization.py to see the resu
     raceline = np.loadtxt(conf.wpt_path, delimiter=";", skiprows=3)
     waypoints = np.array(raceline)
 
+    if rotate_map == True:
+        waypoints[:, 3] += 1.5707963268
+
     if constant_speed:
         # waypoints[:, 5] = waypoints[:, 5] * 2.0
-        waypoints[:, 5] = np.ones((waypoints[:, 5].shape[0],)) * 12.0
+        waypoints[:, 5] = np.ones((waypoints[:, 5].shape[0],)) * constant_speed_value
+    else:
+        waypoints[:, 5] *= velocity_profile_multiplier
 
     # init controllers
     planner_pp = PurePursuitPlanner(conf, 0.805975 + 1.50876)  # 0.805975 + 1.50876
@@ -133,6 +252,12 @@ def main():  # after launching this you can run visualization.py to see the resu
 
     planner_ekin_mpc = STMPCPlanner(model=ExtendedKinematicModel(config=MPCConfigEXT()), waypoints=waypoints,
                                     config=MPCConfigEXT())
+
+    planner_kin_mpc = STMPCPlanner(model=KinematicModel(config=MPCConfigKIN()), waypoints=waypoints,
+                                   config=MPCConfigKIN())
+
+    planner_dyn_mpc = STMPCPlanner(model=DynamicBicycleModel(config=dyn_config), waypoints=waypoints,
+                                   config=dyn_config)
 
     # init graphics
     draw = DrawDebug()
@@ -147,11 +272,11 @@ def main():  # after launching this you can run visualization.py to see the resu
         y = e.cars[0].vertices[1::2]
         top, bottom, left, right = max(y), min(y), min(x), max(x)
         e.score_label.x = left
-        e.score_label.y = top - 700
-        e.left = left - 800
-        e.right = right + 800
-        e.top = top + 800
-        e.bottom = bottom - 800
+        e.score_label.y = top - 700 * 1.3
+        e.left = left - 800 * 1.3
+        e.right = right + 800 * 1.3
+        e.top = top + 800 * 1.3
+        e.bottom = bottom - 800 * 1.3
 
         planner_pp.render_waypoints(e)
         draw.draw_debug(e)
@@ -174,7 +299,7 @@ def main():  # after launching this you can run visualization.py to see the resu
     last_render = 0
 
     # init logger
-    log = {'time': [], 'vx': [], 'v_ref': []}
+    log = {'time': [], 'x': [], 'y': [], 'lap_n': [], 'vx': [], 'v_ref': [], 'tracking_error': []}
 
     # calc number of sim steps per one control step
     num_of_sim_steps = int(control_step / (env.timestep * 1000.0))
@@ -184,14 +309,14 @@ def main():  # after launching this you can run visualization.py to see the resu
     while not done:
 
         # Regulator step MPC
-        vehicle_state = np.array([env.sim.agents[0].state[0],
-                                  env.sim.agents[0].state[1],
+        vehicle_state = np.array([env.sim.agents[0].state[0],  # x
+                                  env.sim.agents[0].state[1],  # y
                                   env.sim.agents[0].state[3],  # vx
                                   env.sim.agents[0].state[4],  # yaw angle
                                   env.sim.agents[0].state[10],  # vy
                                   env.sim.agents[0].state[5],  # yaw rate
                                   env.sim.agents[0].state[2],  # steering angle
-                                  ]) + np.random.randn(7) * 0.0000001
+                                  ]) + np.random.randn(7) * 0.00001
 
         u = [0.0, 0.0]
         if model_to_use == 'pure_pursuit':
@@ -200,10 +325,12 @@ def main():  # after launching this you can run visualization.py to see the resu
                                                  work['tlad'],
                                                  work['vgain'])
 
+            draw.reference_traj_show = np.array([[obs['poses_x'][0]], [obs['poses_y'][0]]]).T
+
             error_steer = steer_angle - env.sim.agents[0].state[2]
             u[1] = 10.0 * error_steer
 
-            error_drive = 13.0 - env.sim.agents[0].state[3]
+            error_drive = speed - env.sim.agents[0].state[3]
             u[0] = 8.0 * error_drive
 
         elif model_to_use == "ext_kinematic":
@@ -214,39 +341,72 @@ def main():  # after launching this you can run visualization.py to see the resu
             # draw predicted states and reference trajectory
             draw.reference_traj_show = np.array([mpc_ref_path_x, mpc_ref_path_y]).T
             draw.predicted_traj_show = np.array([mpc_pred_x, mpc_pred_y]).T
+        elif model_to_use == "kinematic":
+            # change of coordinates from CoG to center of the rear axle
+            x_cog = vehicle_state[0] - planner_kin_mpc.config.LR * np.cos(vehicle_state[3])
+            y_cog = vehicle_state[1] - planner_kin_mpc.config.LR * np.sin(vehicle_state[3])
+
+            u, mpc_ref_path_x, mpc_ref_path_y, mpc_pred_x, mpc_pred_y, mpc_ox, mpc_oy = planner_kin_mpc.plan(
+                np.array([x_cog, y_cog, vehicle_state[2], vehicle_state[3]]))
+            required_steering_angle = u[1]
+            error_steer = required_steering_angle - env.sim.agents[0].state[2]
+            u[1] = 10.0 * error_steer
+
+            # draw predicted states and reference trajectory
+            draw.reference_traj_show = np.array([mpc_ref_path_x, mpc_ref_path_y]).T
+            draw.predicted_traj_show = np.array([mpc_pred_x, mpc_pred_y]).T
+        elif model_to_use == "dynamic":
+            u, mpc_ref_path_x, mpc_ref_path_y, mpc_pred_x, mpc_pred_y, mpc_ox, mpc_oy = planner_dyn_mpc.plan(
+                vehicle_state)
+            u[0] = u[0] / planner_ekin_mpc.config.MASS  # Force to acceleration
+
+            # draw predicted states and reference trajectory
+            draw.reference_traj_show = np.array([mpc_ref_path_x, mpc_ref_path_y]).T
+            draw.predicted_traj_show = np.array([mpc_pred_x, mpc_pred_y]).T
+
+        _, tracking_error, _, _, n_point = nearest_point_on_trajectory(np.array([env.sim.agents[0].state[0], env.sim.agents[0].state[1]]),
+                                                                       np.array([waypoints[:, 1], waypoints[:, 2]]).T)
 
         # set correct friction to the environment
         if use_dyn_friction:
             min_id = get_closest_point_vectorized(np.array([obs['poses_x'][0], obs['poses_y'][0]]), np.array(tpamap))
-            env.params['tire_p_dy1'] = tpadata[str(min_id)][0]  # mu_y
-            env.params['tire_p_dx1'] = tpadata[str(min_id)][0] * 1.1  # mu_x
+            env.params['tire_p_dy1'] = tpadata[str(min_id)][0] * 0.9  # mu_y
+            env.params['tire_p_dx1'] = tpadata[str(min_id)][0]  # mu_x
         else:
-            env.params['tire_p_dy1'] = 0.7  # mu_y
-            env.params['tire_p_dx1'] = 0.8  # mu_x
+            env.params['tire_p_dy1'] = constant_friction * 0.9  # mu_y
+            env.params['tire_p_dx1'] = constant_friction  # mu_x
+
+        # print(env.params['tire_p_dx1'])
+        # print("%f   %f" % (waypoints[:, 5][n_point], env.sim.agents[0].state[3]))
 
         # Simulation step
         step_reward = 0.0
         for i in range(num_of_sim_steps):
-            obs, rew, _, info = env.step(np.array([[u[1], u[0]]]))
+            obs, rew, done, info = env.step(np.array([[u[1], u[0]]]))
             step_reward += rew
+
+            # Rendering
+            last_render += 1
+            if last_render >= render_every:
+                last_render = 0
+                env.render(mode='human_fast')
+
         laptime += step_reward
 
         # Logging
         log['time'].append(laptime)
+        log['x'].append(env.sim.agents[0].state[0])
+        log['y'].append(env.sim.agents[0].state[1])
         log['vx'].append(env.sim.agents[0].state[3])
-        log['v_ref'].append(waypoints[:, 5][0])
+        log['v_ref'].append(waypoints[:, 5][n_point])
+        log['tracking_error'].append(tracking_error)
+        log['lap_n'].append(obs['lap_counts'][0])
 
-        # Rendering
-        last_render += 1
-        if last_render >= render_every:
-            last_render = 0
-            env.render(mode='human_fast')
-
-        if obs['lap_counts'][0] == 1:
+        if obs['lap_counts'][0] == number_of_laps:  # or env.sim.agents[0].state[3] < 0.4 or tracking_error > 10.0:
             done = 1
 
     print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time() - start)
-    with open('log02', 'w') as f:
+    with open('log01_eval', 'w') as f:
         json.dump(log, f)
 
 
--- models/dynamic.py
+++ models/dynamic.py
@@ -1,11 +1,13 @@
 import numpy as np
+from torch.autograd.functional import jacobian
+import torch
 
 
-class ExtendedKinematicModel:
+class DynamicBicycleModel:
     """
     states - [x, y, vx, yaw angle, vy, yaw rate, steering angle]
     inputs - [drive force (proportional to acceleration), steering velocity]
-    reference point - center of rear axle ? Need to check BayesRace paper
+    reference point - CoG
     """
 
     def __init__(self, config):
@@ -35,7 +37,8 @@ class ExtendedKinematicModel:
                                            [np.inf, np.inf]])
         return state_constraints, input_constraints, input_diff_constraints
 
-    def sort_reference_trajectory(self, position_ref, yaw_ref, speed_ref):
+    @staticmethod
+    def sort_reference_trajectory(position_ref, yaw_ref, speed_ref):
         reference = np.array([
             # Sort reference trajectory so the order of reference match the order of the states
             position_ref[:, 0],
@@ -49,13 +52,14 @@ class ExtendedKinematicModel:
         ])
         return reference
 
-    def get_general_states(self, state):
+    @staticmethod
+    def get_general_states(state):
         speed = state[2]
         orientation = state[3]
         position = state[[0, 1]]
         return speed, orientation, position
 
-    def get_f(self, state, control_input):
+    def get_f(self, state, control_input):  # used for forward model propagation
         # state =
         # input = drive force (proportional to acceleration), steering velocity
         # input check
@@ -67,23 +71,117 @@ class ExtendedKinematicModel:
         # states x[k]
         x, y, vx, yaw, vy, yaw_rate, steering_angle = state
 
+        # tire model from: AMZ Driverless: The Full Autonomous Racing System, A simplified Pacejka tire model
+        alfa_f = steering_angle - np.arctan((yaw_rate * self.config.LF + vy) / vx)
+        alfa_r = np.arctan((yaw_rate * self.config.LR - vy) / vx)
+
+        Ffy = self.config.DF * np.sin(self.config.CF * np.arctan(self.config.BF * alfa_f))
+        Fry = self.config.DR * np.sin(self.config.CR * np.arctan(self.config.BR * alfa_r))
+
+        Fx = self.config.CM * Fxr - self.config.CR0 - self.config.CR2 * vx ** 2.0  # https://arxiv.org/pdf/1905.05150.pdf - equation (7)
+        Frx = Fx * (1.0 - self.config.TORQUE_SPLIT)
+        Ffx = Fx * self.config.TORQUE_SPLIT
+
+        # vehicle model from: AMZ Driverless: The Full Autonomous Racing System
         f = np.zeros(7)
+        f[0] = vx * np.cos(yaw) - vy * np.sin(yaw)
+        f[1] = vx * np.sin(yaw) + vy * np.cos(yaw)
+        f[2] = 1.0 / self.config.MASS * (Frx - Ffy * np.sin(steering_angle) + Ffx * np.cos(steering_angle) + vy * yaw_rate * self.config.MASS)
+        f[3] = yaw_rate
+        f[4] = 1.0 / self.config.MASS * (Fry + Ffy * np.cos(steering_angle) + Ffx * np.sin(steering_angle) - vx * yaw_rate * self.config.MASS)
+        f[5] = 1.0 / self.config.I_Z * (Ffy * self.config.LF * np.cos(steering_angle) - Fry * self.config.LR)
+        f[6] = delta_v
+
+        return f
+
+    def get_f_torch_batch(self, state, control_input):  # batch torch implementation used for autograd of dynamics
+        # state =
+        # input = drive force (proportional to acceleration), steering velocity
+        # input check
+
+        # control inputs
+        Fxr = control_input[:, 0]
+        delta_v = control_input[:, 1]
+
+        # states x[k]
+        x = state[:, 0]
+        y = state[:, 1]
+        vx = state[:, 2]
+        yaw = state[:, 3]
+        vy = state[:, 4]
+        yaw_rate = state[:, 5]
+        steering_angle = state[:, 6]
+
+        # tire model from: AMZ Driverless: The Full Autonomous Racing System, A simplified Pacejka tire model
+        alfa_f = steering_angle - torch.arctan((yaw_rate * self.config.LF + vy) / vx)
+        alfa_r = torch.arctan((yaw_rate * self.config.LR - vy) / vx)
+
+        Ffy = self.config.DF * torch.sin(self.config.CF * torch.arctan(self.config.BF * alfa_f))
+        Fry = self.config.DR * torch.sin(self.config.CR * torch.arctan(self.config.BR * alfa_r))
+
+        Fx = self.config.CM * Fxr - self.config.CR0 - self.config.CR2 * vx ** 2.0  # https://arxiv.org/pdf/1905.05150.pdf - equation (7)
+        Frx = Fx * (1.0 - self.config.TORQUE_SPLIT)
+        Ffx = Fx * self.config.TORQUE_SPLIT
+
+        # vehicle model from: AMZ Driverless: The Full Autonomous Racing System
+        f = torch.zeros(state.shape)
+        f[:, 0] = vx * torch.cos(yaw) - vy * torch.sin(yaw)
+        f[:, 1] = vx * torch.sin(yaw) + vy * torch.cos(yaw)
+        f[:, 2] = 1.0 / self.config.MASS * (Frx - Ffy * torch.sin(steering_angle) + Ffx * torch.cos(steering_angle) + vy * yaw_rate * self.config.MASS)
+        f[:, 3] = yaw_rate
+        f[:, 4] = 1.0 / self.config.MASS * (Fry + Ffy * torch.cos(steering_angle) + Ffx * torch.sin(steering_angle) - vx * yaw_rate * self.config.MASS)
+        f[:, 5] = 1.0 / self.config.I_Z * (Ffy * self.config.LF * torch.cos(steering_angle) - Fry * self.config.LR)
+        f[:, 6] = delta_v
 
         return f
 
     def get_model_matrix(self, state, control_input):
-        x, y, vx, yaw, vy, yaw_rate, steering_angle = state
-        Fxr, delta_v = control_input
+        # x, y, vx, yaw, vy, yaw_rate, steering_angle = state
+        # Fxr, delta_v = control_input
+        A_, B_ = jacobian(self.get_f_torch_batch, (torch.from_numpy(state).reshape(1, 7), torch.from_numpy(control_input).reshape(1, 2)))
+        A_ = A_.squeeze().numpy()
+        A_ = np.nan_to_num(A_)
+        B_ = B_.squeeze().numpy()
 
         # State (or system) matrix A, 7x7
-        A = np.zeros((self.config.NXK, self.config.NXK))
+        A = np.diag(np.ones(7)) + A_ * self.config.DTK
 
-        # Input Matrix B; 4x2
-        B = np.zeros((self.config.NXK, self.config.NU))
+        # Input Matrix B; 7x2
+        B = B_ * self.config.DTK
 
-        C = np.zeros(self.config.NXK)
+        C = (np.nan_to_num(self.get_f(state, control_input).reshape(7, 1)) - A_ @ state.reshape(7, 1) - B_ @ control_input.reshape(2, 1)) * self.config.DTK
+        C = C.reshape(7)
 
         return A, B, C
+
+    def batch_get_model_matrix(self, state_vec, control_vec):
+        C_block = []
+
+        state_vec_ = np.moveaxis(state_vec, 0, 1)
+        control_vec_ = np.moveaxis(control_vec, 0, 1)
+
+        def batch_jacobian(f, x):
+            f_sum = lambda x1, x2: torch.sum(f(x1, x2), axis=0)
+            return jacobian(f_sum, x)
+
+        A_, B_ = batch_jacobian(self.get_f_torch_batch, (torch.from_numpy(state_vec_), torch.from_numpy(control_vec_)))
+        A_ = np.nan_to_num(A_.numpy())
+        A_ = np.moveaxis(A_, 1, 0)
+        B_ = B_.numpy()
+        B_ = np.moveaxis(B_, 1, 0)
+
+        # State (or system) matrix A, 7x7
+        A = np.repeat(np.diag(np.ones(7))[:, :, np.newaxis], state_vec_.shape[0], axis=2)
+        A = np.moveaxis(A, -1, 0) + A_ * self.config.DTK
+        B = B_ * self.config.DTK
+
+        for t in range(state_vec.shape[1]):
+            C = (np.nan_to_num(self.get_f(state_vec_[t], control_vec_[t]).reshape(7, 1)) - A_[t] @ state_vec_[t].reshape(7, 1) - B_[t] @ control_vec_[
+                t].reshape(2, 1)) * self.config.DTK
+            C = C.reshape(7)
+            C_block.append(C)
+
+        return list(A), list(B), np.array(C_block)
 
     def predict_motion(self, x0, control_input, dt):
         predicted_states = np.zeros((self.config.NXK, self.config.TK + 1))
@@ -95,3 +193,19 @@ class ExtendedKinematicModel:
             predicted_states[:, i] = state
         input_prediction = np.zeros((2, self.config.TK + 1))
         return predicted_states, input_prediction
+
+    def predict_kin_from_dyn(self, states, x0):
+        # states - [x, y, vx, yaw angle, vy, yaw rate, steering angle]
+        states[:, 0][0] = x0[0]  # x
+        states[:, 0][1] = x0[1]  # y
+        states[:, 0][3] = x0[3]  # yaw
+
+        for i in range(1, states.shape[1]):
+            states[:, i][0] = states[:, i - 1][0] + (
+                    states[:, i - 1][2] * np.cos(states[:, i - 1][3]) - states[:, i - 1][4] * np.sin(
+                states[:, i - 1][3])) * self.config.DTK
+            states[:, i][1] = states[:, i - 1][1] + (
+                    states[:, i - 1][2] * np.sin(states[:, i - 1][3]) + states[:, i - 1][4] * np.cos(
+                states[:, i - 1][3])) * self.config.DTK
+            states[:, i][3] = states[:, i - 1][3] + (states[:, i - 1][5]) * self.config.DTK
+        return states
