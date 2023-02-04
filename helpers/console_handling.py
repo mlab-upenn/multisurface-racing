5b30a2be0d6bba433aa4e96f1031a008d6e7230a
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-02-04 18:58:17 -0500

    Add console handling definitions for easy color change

---
+++ helpers/console_handling.py
@@ -0,0 +1,14 @@
+color_map = {
+    "red": "\033[91m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "blue": "\033[94m",
+    "purple": "\033[95m",
+    "cyan": "\033[96m",
+    "white": "\033[97m",
+    "reset": "\033[0m",
+    "bold": "\033[1m",
+    "underline": "\033[4m",
+    "blink": "\033[5m",
+    "reverse": "\033[7m"
+}
